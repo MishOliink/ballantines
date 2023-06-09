c5564141e2421a308c8c92e230b2333812a6a6b4
gpo993 <gpo993it@gmail.com>
2023-06-09 21:16:23 +0300

    Business card site
    Adaptive

--- js/script.js
+++ js/script.js
@@ -2,6 +2,14 @@ $(function(){
    $('.slider').slick({
       arrows: false,
       dots: true,
-      speed: 500
+      speed: 700
+   });
+
+    // Menu nav toggle
+    $('#nav_toggle').on('click', function(event) {
+      event.preventDefault();
+
+      $(this).toggleClass('active');
+      $('#menu').toggleClass('active');
    });
 });
